c5af7b0fcfe3d657839007c2f318ddab87f71708
moner050 <minhyung0237@gmail.com>
2023-08-02 15:15:32 +0900

    Feat: Basic Be 프로젝트 생성
    기초 연습용 프로젝트 생성

---
+++ src/main/java/com/whatev/basicbe/BasicbeApplication.java
@@ -0,0 +1,13 @@
+package com.whatev.basicbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whatev/basicbe/BasicbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatev.basicbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
