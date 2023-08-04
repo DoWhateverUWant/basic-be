7cb80e8dd07b99eccd412f17cc0e05dd07b1822a
moner050 <minhyung0237@gmail.com>
2023-08-04 17:29:25 +0900

    Feat: UserAccountRepository 패키지 v1으로 이동

--- src/main/java/com/whatev/basicbe/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.whatev.basicbe.repository;
-
-import com.whatev.basicbe.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
-}
