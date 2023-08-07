dd832651e2ab036b5f17e0a43379ab69aa3fb4ec
moner050 <minhyung0237@gmail.com>
2023-08-07 15:24:25 +0900

    Feat: 회원가입 및 로그인 리턴타입 변경, 회원조회 추가

--- src/main/java/com/whatev/basicbe/app/v1/service/UserAccountServiceV1.java
+++ src/main/java/com/whatev/basicbe/app/v1/service/UserAccountServiceV1.java
@@ -1,13 +1,17 @@
 package com.whatev.basicbe.app.v1.service;
 
-import com.whatev.basicbe.app.v1.dto.UserAccountDtoV1;
-import com.whatev.basicbe.app.v1.dto.request.LoginRequest;
-import com.whatev.basicbe.app.v1.dto.response.LoginResponse;
+import com.whatev.basicbe.app.v1.domain.UserAccount;
+import com.whatev.basicbe.app.v1.dto.request.UserAccountRequestV1;
+import com.whatev.basicbe.app.v1.dto.request.LoginRequestV1;
+import com.whatev.basicbe.app.v1.dto.response.LoginResponseV1;
+import com.whatev.basicbe.app.v1.dto.response.UserAccountResponseV1;
 import com.whatev.basicbe.app.v1.repository.UserAccountChangedInfoRepositoryV1;
 import com.whatev.basicbe.app.v1.repository.UserAccountRepositoryV1;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class UserAccountServiceV1 {
@@ -17,23 +21,32 @@ public class UserAccountServiceV1 {
 
     // 회원 가입
     @Transactional
-    public void saveUser(UserAccountDtoV1 userAccountDtoV1) {
+    public UserAccountResponseV1 saveUser(UserAccountRequestV1 userAccountRequestV1) {
         // 비밀 번호 체크
-        if(!userAccountDtoV1.getUserPassword().equals(userAccountDtoV1.getUserConfirmPassword())) {
+        if(!userAccountRequestV1.getUserPassword().equals(userAccountRequestV1.getUserConfirmPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
         // 회원 정보 저장 및 로그용 DB에 저장
-        userAccountRepositoryV1.save(userAccountDtoV1.toEntity());
-        userAccountChangedInfoRepositoryV1.save(userAccountDtoV1.toInfoEntity());
+        UserAccount user = userAccountRepositoryV1.save(userAccountRequestV1.toEntity());
+//        userAccountChangedInfoRepositoryV1.save(userAccountRequestV1.toInfoEntity());
+
+        return UserAccountResponseV1.from(user);
     }
 
     // 로그인
     @Transactional(readOnly = true)
-    public LoginResponse login(LoginRequest loginRequest) {
-        return userAccountRepositoryV1.findById(loginRequest.getUserId())
-                .filter(user -> user.getUserPassword().equals(loginRequest.getUserPassword()))
-                .map(LoginResponse::from)
+    public LoginResponseV1 login(LoginRequestV1 loginRequestV1) {
+        return userAccountRepositoryV1.findById(loginRequestV1.getUserId())
+                .filter(user -> user.getUserPassword().equals(loginRequestV1.getUserPassword()))
+                .map(LoginResponseV1::from)
                 .orElseThrow(() -> new EntityNotFoundException("아이디 또는 비밀번호가 일치하지 않습니다."));
+    }
+
+    // 회원조회 (userId)
+    @Transactional(readOnly = true)
+    public Optional<UserAccountResponseV1> searchUser(String userId) {
+        return userAccountRepositoryV1.findById(userId)
+                .map(UserAccountResponseV1::from);
     }
 }
