13db09b542cca166fbbf8afd317e33cb5ba54294
moner050 <minhyung0237@gmail.com>
2023-08-09 16:24:51 +0900

    Feat: LoginResponseV1 -> UserAccountResponseV1 로 변경

--- src/main/java/com/whatev/basicbe/app/v1/controller/MainControllerV1.java
+++ src/main/java/com/whatev/basicbe/app/v1/controller/MainControllerV1.java
@@ -1,6 +1,6 @@
 package com.whatev.basicbe.app.v1.controller;
 
-import com.whatev.basicbe.app.v1.dto.response.LoginResponseV1;
+import com.whatev.basicbe.app.v1.dto.response.UserAccountResponseV1;
 import com.whatev.basicbe.app.v1.session.SessionConst;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,12 +15,12 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class MainControllerV1 {
 
     @GetMapping(path = "/")
-    public ResponseEntity<LoginResponseV1> homeLogin(
-            @SessionAttribute(name = SessionConst.LOGIN_USER, required = false) LoginResponseV1 loginResponseV1) throws HttpSessionRequiredException {
-        if(loginResponseV1 == null) {
+    public ResponseEntity<UserAccountResponseV1> homeLogin(
+            @SessionAttribute(name = SessionConst.LOGIN_USER, required = false) UserAccountResponseV1 userAccountResponseV1) throws HttpSessionRequiredException {
+        if(userAccountResponseV1 == null) {
             throw new HttpSessionRequiredException("로그인 정보가 없습니다.");
         }
 
-        return ResponseEntity.ok(loginResponseV1);
+        return ResponseEntity.ok(userAccountResponseV1);
     }
 }
