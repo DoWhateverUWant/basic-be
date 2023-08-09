55b36fa178feff2947447e9569acf10febc11e09
moner050 <minhyung0237@gmail.com>
2023-08-09 16:26:19 +0900

    Feat: 내 정보 조회 메서드 수정, 내 정보 수정 메서드 추가

--- src/main/java/com/whatev/basicbe/app/v1/controller/UserAccountControllerV1.java
+++ src/main/java/com/whatev/basicbe/app/v1/controller/UserAccountControllerV1.java
@@ -1,14 +1,15 @@
 package com.whatev.basicbe.app.v1.controller;
 
 import com.whatev.basicbe.app.v1.dto.request.LoginRequestV1;
+import com.whatev.basicbe.app.v1.dto.request.UpdateUserAccountRequest;
 import com.whatev.basicbe.app.v1.dto.request.UserAccountRequestV1;
-import com.whatev.basicbe.app.v1.dto.response.LoginResponseV1;
 import com.whatev.basicbe.app.v1.dto.response.UserAccountResponseV1;
 import com.whatev.basicbe.app.v1.service.UserAccountServiceV1;
 import com.whatev.basicbe.app.v1.session.SessionConst;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -34,15 +35,15 @@ public class UserAccountControllerV1 {
     // 로그인
     @PostMapping(path = "/login")
     public String login(@RequestBody LoginRequestV1 loginRequestV1, HttpServletRequest request, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        LoginResponseV1 loginResponseV1 = userAccountServiceV1.login(loginRequestV1);
+        UserAccountResponseV1 userAccountResponseV1 = userAccountServiceV1.login(loginRequestV1);
 
-        if(loginResponseV1 == null) {
+        if (userAccountResponseV1 == null) {
             bindingResult.reject("Login Fail", "아이디 또는 비밀번호가 일치하지 않습니다.");
             return "redirect:/api/v1";
         }
 
         HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.LOGIN_USER, loginResponseV1);
+        session.setAttribute(SessionConst.LOGIN_USER, userAccountResponseV1);
         session.setMaxInactiveInterval(7200);
 
         redirectAttributes.addAttribute("STATUS", "200");
@@ -56,7 +57,7 @@ public class UserAccountControllerV1 {
     public String logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
 
-        if(session != null) {
+        if (session != null) {
             session.invalidate();
         }
 
@@ -65,13 +66,31 @@ public class UserAccountControllerV1 {
 
     // 내 정보 조회
     @GetMapping(path = "/my-info")
-    public ResponseEntity<LoginResponseV1> myInfo(
-            @SessionAttribute(name = SessionConst.LOGIN_USER, required = false) LoginResponseV1 loginResponseV1) throws HttpSessionRequiredException {
+    public ResponseEntity<UserAccountResponseV1> myInfo(
+            @SessionAttribute(name = SessionConst.LOGIN_USER, required = false) UserAccountResponseV1 userAccountResponseV1) throws HttpSessionRequiredException {
 
-        if(loginResponseV1 == null) {
+        if (userAccountResponseV1 == null) {
             throw new HttpSessionRequiredException("로그인 정보가 없습니다.");
         }
 
-        return ResponseEntity.ok().body(loginResponseV1);
+        return ResponseEntity.ok().body(userAccountResponseV1);
+    }
+
+    // 내 정보 수정
+    @PutMapping(path = "/my-info")
+    public ResponseEntity<?> updateMyInfo(@RequestBody UpdateUserAccountRequest userAccountRequest, HttpServletRequest request,
+                                          @SessionAttribute(name = SessionConst.LOGIN_USER, required = false) UserAccountResponseV1 responseV1) throws HttpSessionRequiredException {
+
+        if (responseV1 == null) {
+            throw new HttpSessionRequiredException("로그인 정보가 없습니다.");
+        }
+
+        UserAccountResponseV1 userAccountResponseV1 = userAccountServiceV1.updateUser(responseV1.getUserId(), userAccountRequest);
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_USER, userAccountResponseV1);
+        session.setMaxInactiveInterval(7200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userAccountResponseV1);
     }
 }
