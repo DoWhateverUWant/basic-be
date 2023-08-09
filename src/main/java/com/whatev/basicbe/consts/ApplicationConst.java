bf70896c510c18e10d488f9c838f47574baef738
moner050 <minhyung0237@gmail.com>
2023-08-09 16:59:08 +0900

    Feat: 패키지 경로들을 상수로 지정한 ApplicationConst 클래스 생성

---
+++ src/main/java/com/whatev/basicbe/consts/ApplicationConst.java
@@ -0,0 +1,7 @@
+package com.whatev.basicbe.consts;
+
+public class ApplicationConst {
+    public static final String V1 = "com.whatev.basicbe.app.v1";
+    public static final String V2 = "com.whatev.basicbe.app.v2";
+    public static final String V3 = "com.whatev.basicbe.app.v3";
+}
